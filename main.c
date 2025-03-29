c74880934a66bf34746f5940589dee6115c0b41e
ruizmaa <ruizma31@gmail.com>
2025-03-29 22:19:06 +0000

    Changed files to alow debug and test castling

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "board.h"
 #include "game.h"
+#include <stdio.h>
 
 int main() {
     // Creacion del tablero
     ChessBoard board;
-    //initialize_board(&board);
+    // initialize_board(&board);
 
-    initialize_custom_board_from_file(&board, "src/tests/rook.txt");
+    initialize_custom_board_from_file(&board, "src/tests/castling/long_black_ok.txt");
 
     while (1) {
         play_turn(&board);
